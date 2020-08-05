afd517cbf13f1490ce86385c6b56b4bbc29ec3a2
hardik <hardikdudhrejiya07@gmail.com>
2020-08-05 10:41:25 +0530

    Initial Commit

---
+++ BS TASK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BS TASK
+//
+//  Created by Hardik on 8/5/20.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
